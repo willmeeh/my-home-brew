0bb3d69068a0a0a2945eb35c7ed37e9ac8d9e746
William <william.lucas.mehler@gmail.com>
2019-04-04 22:48:50 -0300

    Updates

---
+++ precache-manifest.8ad3d891671bc58ce95d0fe0342925d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "658c28a9fedf5ae701fe",
+    "url": "/my-home-brew/static/js/runtime~main.36a0bf9a.js"
+  },
+  {
+    "revision": "2a677def91c126fc7fcf",
+    "url": "/my-home-brew/static/js/main.03d9f48c.chunk.js"
+  },
+  {
+    "revision": "ab9f7fc409e9c96a2938",
+    "url": "/my-home-brew/static/js/2.dc165434.chunk.js"
+  },
+  {
+    "revision": "2a677def91c126fc7fcf",
+    "url": "/my-home-brew/static/css/main.39096863.chunk.css"
+  },
+  {
+    "revision": "ab9f7fc409e9c96a2938",
+    "url": "/my-home-brew/static/css/2.23af97d5.chunk.css"
+  },
+  {
+    "revision": "ec48f8544701021e2a94f89f3e8280d3",
+    "url": "/my-home-brew/index.html"
+  }
+];
