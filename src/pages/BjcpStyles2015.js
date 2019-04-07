10e48d77df09df2310acda032e9baeb41056e0b8
William <william.lucas.mehler@gmail.com>
2019-04-07 20:37:18 -0300

    Exibir estilos, de acordo com a categoria
    Signed-off-by: William <william.lucas.mehler@gmail.com>

--- src/pages/BjcpStyles2015.js
+++ src/pages/BjcpStyles2015.js
@@ -1,41 +1,64 @@
 import React, { Component, Fragment } from 'react'
 import { withRouter } from "react-router-dom";
-import { Layout, Typography, Anchor } from 'antd';
+import { Layout, Menu } from 'antd';
 
 import { createStylesDescription } from 'components/BeerStyleDescription'
 import bjcpStyles from 'data/bjcp-2015-guideliness-pt-br.min.json';
 
+const SubMenu = Menu.SubMenu;
 const { Content, Sider } = Layout;
-const { Title } = Typography;
-const { Link: AnchorLink } = Anchor;
 
 class BjcpStyles2015 extends Component {
-    render() {
-        let styles = [];
-        bjcpStyles.forEach(category => {
-           styles = [ ...styles, ...category.styles ];
-        });
+    state = { selectedStyle: null }
 
-        console.log('styles', styles)
+    handleClick = (e) => {
+        const { key, keyPath } = e;
+        if (key === 'guia') {
+            this.setState({ selectedStyle: null });
+        }
+        else {
+            const category = bjcpStyles.find((item) => item.categoryName === keyPath[1]);
+            if (category && category.styles) {
+                const style = category.styles.find((item) => item.styleName === key);
+                if (style) {
+                    this.setState({ selectedStyle: style });
+                }
+            }
+        }
+    }
+
+    createCategoriesMenu(categories) {
+        return categories.map(({ categoryName, styles }) => {
+            return (
+                <SubMenu key={categoryName} title={categoryName}>
+                    {styles.map(({ styleName }) => (
+                        <Menu.Item key={styleName}>{styleName}</Menu.Item>
+                    ))}
+                </SubMenu>
+            )
+        });
+    }
+
+    render() {
+        const { selectedStyle } = this.state;
 
         return (
             <Fragment>
-                <Title className="text-center">Estilos BJCP 2015</Title>
                 <Layout className="page-layout">
-                    <Content className="page-content">
-                        {createStylesDescription(styles)}
-                    </Content>
-                    <Sider width={200} className="page-sider">
-                        <Anchor offsetTop={64}>
-                            {styles.map((style, index) => (
-                                <AnchorLink 
-                                    key={index}
-                                    href={'#anchor' + index} 
-                                    title={style.styleName} 
-                                />
-                            ))}
-                        </Anchor>
+                    <Sider width={256} className="page-sider">
+                        <Menu
+                            onClick={this.handleClick}
+                            defaultSelectedKeys={["guia"]}
+                            style={{ width: 256 }}
+                            mode="inline"
+                        >
+                            <Menu.Item key="guia">Guia</Menu.Item>
+                            {this.createCategoriesMenu(bjcpStyles)}
+                        </Menu>
                     </Sider>
+                    <Content className="page-content">
+                        {selectedStyle ? createStylesDescription([selectedStyle]) : '@TODO: Adicionar componente com o guia de estilos'}
+                    </Content>
                 </Layout>
             </Fragment>
         );
